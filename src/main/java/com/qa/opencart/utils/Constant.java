ed6f2e87ad39780ea6dffdec6b3661f5753d5721
Anand Singh1 <AnandSingh@fsoft4pdc.com>
2021-03-18 12:23:20 +0530

    fixed the code

--- src/main/java/com/qa/opencart/utils/Constant.java
+++ src/main/java/com/qa/opencart/utils/Constant.java
@@ -5,7 +5,7 @@ public class Constant {
 	public static final String LOGIN_PAGE_TITLE="Account Login";
 	public static final String ACCOUNT_PAGE_TITLE="My Account";
 	public static final int ACCOUNT_PAGE_COUNT =4;
-	public static final int PRODUCT_IMAGES_COUNT_MACBOOK =5;
+	public static final int PRODUCT_IMAGES_COUNT_MACBOOK =4;
 	public static final String PRODUCT_HEADER_TEXT ="MacBook Pro";
 	public static final String ADD_TO_CART_SUCCESS_MSG ="Success: You have added MacBook Pro to your shopping cart!";
 	public static final String ACCOUNT_CREATION_SUCCESS_MSG ="Your Account Has Been Created!";
